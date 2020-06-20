db7d71e70fe8a3d72dabc141859bc9f6abcaf66c
Lv Zheng <zhenglv@hotmail.com>
2020-06-20 21:07:46 +0800

    Add sdfirm support as BBL
    This patch adds BBL alternative of sdfirm.
    sdfirm can support BBL mode for its spike/qemu BSPs. This patch
    adds BBL mode switch via the following environments:
     BBL: BBL type, currently riscv-pk or sdfirm
     MACH: sdfirm BSP type, currently spike64 or virt64
     SDFIRM_DIR: sdfirm source directorty
    
    Usage examples of sdfirm with spike:
     tiny-linux> LINUX_DIR=../linux BUSYBOX_DIR=../busybox SDFIRM_DIR=../sdfirm BBL=sdfirm MACH=spike64 ./script/build.sh
     tiny-linux> BBL=sdfirm ./script/run-spike.sh
    Usage examples of sdfirm with qemu/virt:
     tiny-linux> LINUX_DIR=../linux BUSYBOX_DIR=../busybox SDFIRM_DIR=../sdfirm BBL=sdfirm MACH=virt64 ./script/build.sh
     tiny-linux> BBL=sdfirm ./script/run-qemu.sh
    
    Signed-off-by: Lv Zheng <zhenglv@hotmail.com>

--- script/run-qemu.sh
+++ script/run-qemu.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 QEMU=qemu-system-riscv64
+
+if [ -z $ARCH ]; then
+	ARCH=riscv
+fi
+if [ -z $BBL ]; then
+	BBL=riscv-pk
+fi
+if [ "x$BBL" = "xsdfirm" ]; then
+	BBL_ELF=obj/sdfirm-${ARCH}/sdfirm
+fi
+if [ "x$BBL" = "xriscv-pk" ]; then
+	BBL_ELF=obj/bbl/bbl
+fi
+
 which $QEMU
 if [ $? -eq 0 ]
 then
-	$QEMU -nographic -machine virt -bios obj/bbl/bbl
+	$QEMU -nographic -machine virt -bios $BBL_ELF
 else
 	echo "No QEMU found"
 fi
--- script/run-spike.sh
+++ script/run-spike.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+ARCH=riscv
 SPIKE=spike
 SMP=4
+
+if [ -z $BBL ]; then
+	BBL=riscv-pk
+fi
+if [ "x$BBL" = "xsdfirm" ]; then
+	BBL_ELF=obj/sdfirm-${ARCH}/sdfirm
+fi
+if [ "x$BBL" = "xriscv-pk" ]; then
+	BBL_ELF=obj/bbl/bbl
+fi
+
 which $SPIKE
 if [ $? -eq 0 ]
 then
-	$SPIKE -p$SMP obj/bbl/bbl
+	$SPIKE -p$SMP $BBL_ELF
 else
 	echo "No Spike found"
 fi
