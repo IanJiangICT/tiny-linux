6a4a76b5c2b36d814ac57583c05b3a09fac2c740
Ian Jiang <ianjiang.ict@gmail.com>
2020-12-30 09:48:33 +0800

    bench: Extend t_time

--- bench/t_time.c
+++ bench/t_time.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
 	long long int value;
 	long long int i;
 	long long int loop_cnt;
-	clock_t ts0;
-	clock_t ts1;
-	clock_t ts2;
-	clock_t ts3;
-	clock_t ts4;
+	clock_t clk0;
+	clock_t clk1;
+	clock_t clk2;
+	clock_t clk3;
+	clock_t clk4;
+	struct timeval tv0;
+	struct timeval tv1;
+	struct timeval tv2;
+	struct timeval tv3;
+	struct timeval tv4;
 
-	ts0 = clock();
+	clk0 = clock();
+	gettimeofday(&tv0, NULL);
 	loop_cnt = 10000*10;
 	printf("loop count = %lld\n", loop_cnt);
 	value = 0;
@@ -21,7 +28,8 @@ int main(int argc, char **argv)
 		value += i;
 	}
 
-	ts1 = clock();
+	clk1 = clock();
+	gettimeofday(&tv1, NULL);
 	loop_cnt *= 10;
 	printf("loop count = %lld\n", loop_cnt);
 	value = 0;
@@ -29,7 +37,8 @@ int main(int argc, char **argv)
 		value += i;
 	}
 
-	ts2 = clock();
+	clk2 = clock();
+	gettimeofday(&tv2, NULL);
 	loop_cnt *= 10;
 	printf("loop count = %lld\n", loop_cnt);
 	value = 0;
@@ -37,7 +46,8 @@ int main(int argc, char **argv)
 		value += i;
 	}
 
-	ts3 = clock();
+	clk3 = clock();
+	gettimeofday(&tv3, NULL);
 	loop_cnt *= 10;
 	printf("loop count = %lld\n", loop_cnt);
 	value = 0;
@@ -45,11 +55,13 @@ int main(int argc, char **argv)
 		value += i;
 	}
 
-	ts4 = clock();
-	printf("ts0 = %ld\n", ts0);
-	printf("ts1 = %ld\n", ts1);
-	printf("ts2 = %ld\n", ts2);
-	printf("ts3 = %ld\n", ts3);
-	printf("ts4 = %ld\n", ts4);
+	clk4 = clock();
+	gettimeofday(&tv4, NULL);
+
+	printf("clk = %ld, tv = %ld %ld\n", clk0, tv0.tv_sec, tv0.tv_usec);
+	printf("clk = %ld, tv = %ld %ld\n", clk1, tv1.tv_sec, tv1.tv_usec);
+	printf("clk = %ld, tv = %ld %ld\n", clk2, tv2.tv_sec, tv2.tv_usec);
+	printf("clk = %ld, tv = %ld %ld\n", clk3, tv3.tv_sec, tv3.tv_usec);
+	printf("clk = %ld, tv = %ld %ld\n", clk4, tv4.tv_sec, tv4.tv_usec);
 	return 0;
 }
