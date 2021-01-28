dfdf2ea05a72dc8d6b773968f5495f6d3df004ec
Ian Jiang <ianjiang.ict@gmail.com>
2021-01-28 15:17:19 +0800

    arm: Fix Linux running script for QEMU

--- script/run-qemu-linux-arm-vexpress-a9.sh
+++ script/run-qemu-linux-arm-vexpress-a9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 qemu-system-arm -nographic -machine vexpress-a9 -m 512M \
-	-kernel obj/linux-arm/arch/arm/boot/zImage \ 
+	-kernel obj/linux-arm/arch/arm/boot/zImage \
 	-dtb obj/linux-arm/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
 	-append "console=ttyAMA0"
 
