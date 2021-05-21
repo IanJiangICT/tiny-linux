cc671ed49ee97b8298aac3cf276cd81431db57d9
Ian Jiang <ianjiang.ict@gmail.com>
2021-05-21 17:39:45 +0800

    bench: Add sleep test

---
+++ bench/t_sleep.c
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
+#define __USE_GNU
+#include <sched.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/syscall.h>
+
+void usage(void)
+{
+	printf("Usage:\n");
+	printf("  t_sleep time-in-ms cpu-id\n");
+	printf("Example:\n");
+	printf("  t_sleep 10 2\n");
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	unsigned long cnt = 0;
+	int ms_cnt = 0;
+	int cpu_id = 0;
+	cpu_set_t mask;
+	unsigned cpu, node;
+	int i;
+	int ret;
+
+	if (argc != 3) {
+		usage();
+		return 0;
+	}
+
+	ms_cnt = atoi(argv[1]);
+	cpu_id = atoi(argv[2]);
+
+	if (ms_cnt < 0 || ms_cnt > 1000) {
+		ms_cnt = 0;
+	}
+	if (cpu_id < 0 || cpu_id > 32) {
+		cpu_id = 0;
+	}
+
+
+	printf("Test sleep for %d ms at CPU %d\n", ms_cnt, cpu_id);
+
+	CPU_ZERO(&mask);
+	CPU_SET(cpu_id, &mask);
+	ret = sched_setaffinity(0, sizeof(mask), &mask);
+	if (ret == -1) {
+		printf("Error: Failed to set affinity. ret = %d\n", ret);
+		return 0;
+	}
+
+	syscall(SYS_getcpu, &cpu, &node, NULL);
+	printf("T cpu %u cnt %lu\n", cpu, cnt);
+	cnt++;
+	for (i = 0; i < 0x1000; i++) {;}
+	if (ms_cnt > 0)
+		usleep(ms_cnt * 1000);
+
+	return 0;
+}
