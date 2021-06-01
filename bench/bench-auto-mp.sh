4a7ef8d455c83dd00654e5eccc92290e84d95a0f
Ian Jiang <ianjiang.ict@gmail.com>
2021-06-01 12:40:51 +0800

    bench: Define processes count based on CPU count

--- bench/bench-auto-mp.sh
+++ bench/bench-auto-mp.sh
@@ -1,18 +1,18 @@
 bench_dir=$1
-#mp_cnt=`grep processor /proc/cpuinfo | wc -l`
-mp_cnt=$2
+mp_cnt=`grep processor /proc/cpuinfo | wc -l`
 
 echo "Start running benches under $bench_dir mp_cnt = $mp_cnt"
 
-for i in 1 2 3 4 5 6 7 8 ;
+for_list=`seq 1 $mp_cnt`
+for i in $for_list
 do
 	(sh $bench_dir/bench-auto.sh $bench_dir $i > /bench-auto-$i.log &)
-	
 done
+
 while (``);
 do
 	echo "----"
-	cat /proc/sched_debug | grep -e ^cpu\# -e ^\>R -e ^\ S -e riscv | cut -b1-40
+	cat /proc/sched_debug | grep -e ^cpu\# -e ^\>R -e riscv | cut -b1-40
 	ls -l /bench-auto-*.log
 	usleep 200000
 done
